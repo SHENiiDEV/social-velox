4339883f15fb4c6add1204bac0b7a825967086d7
Ambex Developer <developer.7n5x@ambex.lv>
2026-09-08 16:47:49 +0300

    Add Velox Play favicons (favicon.ico, favicon.png) and header tags

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,11 @@
     <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <title inertia>{{ config('app.name', 'Velox Play') }}</title>
 
+    <!-- Favicon -->
+    <link rel="icon" type="image/png" href="/favicon.png">
+    <link rel="shortcut icon" href="/favicon.ico">
+    <link rel="apple-touch-icon" href="/favicon.png">
+
     <!-- Inter / JetBrains Mono Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
