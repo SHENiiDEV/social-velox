7ece16c90015ec13792d402de3643cf6926c5cae
Ambex Developer <developer.7n5x@ambex.lv>
2026-09-08 16:24:33 +0300

    Update logo to ultra-clean borderless cyan vector design

--- resources/js/Components/ObsidianLogo.jsx
+++ resources/js/Components/ObsidianLogo.jsx
@@ -6,7 +6,7 @@ export default function ObsidianLogo({ className = 'h-10 sm:h-12 lg:h-14' }) {
       <img
         src="/images/logo.jpg"
         alt="Velox Play"
-        className={`${className} w-auto object-contain mix-blend-screen group-hover:scale-105 transition-transform duration-300 drop-shadow-[0_0_12px_rgba(6,182,212,0.6)]`}
+        className={`${className} w-auto object-contain mix-blend-screen group-hover:scale-105 transition-transform duration-300`}
       />
     </div>
   );
